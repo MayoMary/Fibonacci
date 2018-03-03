5f8166516742ced310e5fc528417ed757efc65b0
MayoMary <36369533+MayoMary@users.noreply.github.com>
2018-03-03 20:27:36 +0000

    Update fib.py
    Updated with the wording as per used in my answer in the discussion forum.

--- fib.py
+++ fib.py
@@ -1,6 +1,9 @@
 # Ian McLoughlin
 # A program that displays Fibonacci numbers.
 # Exercise 1
+# My name is Mary, so the first and last letters of my name (M + Y =13 + 25) give the number 38. The 38th Fibonacci number is 39088169.
+
+
 
 def fib(n):
   """This function returns the nth Fibonacci number."""
@@ -24,6 +27,10 @@ print("Fibonacci number", x, "is", ans)
 # Ian McLoughlin
 # A program that displays Fibonacci numbers.
 # Exercise 2
+# My surname is McHale.
+#The first letter is M and the number is 77.
+# The last letter is e and the number is 101.
+# Script returms the Fibonacci number for 178. 
 
 def fib(n):
   """This function returns the nth Fibonacci number."""
